91c4d68401a294a1f703201075d98e3304e36063
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-08-13 17:09:02 +0300

    Реализует мутации модуля Builder

--- src/frontend/src/store/modules/builder.store.js
+++ src/frontend/src/store/modules/builder.store.js
@@ -5,6 +5,14 @@ import {
   normalizeSize,
   normalizeSauce,
 } from "@/common/helpers";
+import {
+  SET_PIZZA_NAME,
+  SET_PIZZA_DOUGH,
+  SET_PIZZA_SIZE,
+  SET_PIZZA_SAUCE,
+  SET_PIZZA_INGREDIENT,
+  MOVE_PIZZA_INGREDIENT,
+} from "@/store/mutations-types";
 
 const setupState = () => {
   const doughs = pizza.dough.map(normalizeDough);
@@ -56,5 +64,43 @@ export default {
     selectedIngredients: (state) => state.pizza.ingredients,
     selectedSauce: (state) => state.pizza.sauce,
     selectedSize: (state) => state.pizza.size,
+  },
+  mutations: {
+    [SET_PIZZA_NAME](state, name) {
+      state.pizza.name = name;
+    },
+    [SET_PIZZA_DOUGH](state, dough) {
+      state.pizza.dough = dough;
+    },
+    [SET_PIZZA_SIZE](state, size) {
+      state.pizza.size = size;
+    },
+    [SET_PIZZA_SAUCE](state, sauce) {
+      state.pizza.sauce = sauce;
+    },
+    [SET_PIZZA_INGREDIENT](state, { ingredient, count }) {
+      const ingredients = [...state.pizza.ingredients];
+      const index = ingredients.findIndex(({ id }) => id === ingredient.id);
+
+      if (count > 0) {
+        ~index
+          ? (ingredients[index].count = count)
+          : ingredients.push({ ...ingredient, count });
+      } else {
+        ingredients.splice(index, 1);
+      }
+
+      state.pizza.ingredients = ingredients;
+    },
+    [MOVE_PIZZA_INGREDIENT](state, ingredient) {
+      const ingredients = [...state.pizza.ingredients];
+      const index = ingredients.findIndex(({ id }) => id === ingredient.id);
+
+      ~index
+        ? (ingredients[index].count = ingredients[index].count + 1)
+        : ingredients.push({ ...ingredient, count: 1 });
+
+      state.pizza.ingredients = ingredients;
+    },
   },
 };
