3d62e4090e819cce693b18c21de78e818ed4af91
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-08-13 15:26:15 +0300

    Реализует состояние модуля Builder

--- src/frontend/src/store/modules/builder.store.js
+++ src/frontend/src/store/modules/builder.store.js
@@ -1,1 +1,34 @@
-export default {};
+import pizza from "@/static/pizza.json";
+import {
+  normalizeDough,
+  normalizeIngredient,
+  normalizeSize,
+  normalizeSauce,
+} from "@/common/helpers";
+
+const setupState = () => {
+  const doughs = pizza.dough.map(normalizeDough);
+  const ingredients = pizza.ingredients.map(normalizeIngredient);
+  const sizes = pizza.sizes.map(normalizeSize);
+  const sauces = pizza.sauces.map(normalizeSauce);
+
+  return {
+    doughs,
+    ingredients,
+    sizes,
+    sauces,
+
+    pizza: {
+      name: "",
+      dough: doughs[0],
+      size: sizes[1],
+      sauce: sauces[0],
+      ingredients: [],
+    },
+  };
+};
+
+export default {
+  namespaced: true,
+  state: setupState(),
+};
