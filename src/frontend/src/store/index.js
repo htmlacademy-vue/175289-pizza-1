deb5a979bf868977f31e5b968ed5acc7683d631f
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-08-13 14:47:01 +0300

    Подключает модули к хранилищу

--- src/frontend/src/store/index.js
+++ src/frontend/src/store/index.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import Auth from "@/store/modules/auth.store";
+import Builder from "@/store/modules/builder.store";
+import Cart from "@/store/modules/cart.store";
+import Orders from "@/store/modules/orders.store";
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+  modules: {
+    Auth,
+    Builder,
+    Cart,
+    Orders,
+  },
+});
