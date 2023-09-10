c35aa8683956fd33c987309259ca449463d21b45
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-09-10 23:12:18 +0300

    Обновляет значения выпадающего списка

--- src/frontend/src/modules/cart/components/CartForm.vue
+++ src/frontend/src/modules/cart/components/CartForm.vue
@@ -5,9 +5,9 @@
         <span class="cart-form__label">Получение заказа:</span>
 
         <select name="test" class="select">
-          <option value="1">Заберу сам</option>
+          <option value="1">Получу сам</option>
           <option value="2">Новый адрес</option>
-          <option value="3">Дом</option>
+          <option value="3">Существующий адрес</option>
         </select>
       </label>
 
