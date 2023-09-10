6f53d9a469c5d79a0fb9265ceb3c5215aceefa1b
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-09-10 23:06:29 +0300

    Добавляет AppInput

---
+++ src/frontend/src/common/components/AppInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <label class="input" :class="{ 'input--big-label': bigLabel }">
+    <span>{{ label }}{{ bigLabel ? ":" : "" }}{{ required ? "*" : "" }}</span>
+    <input
+      type="text"
+      :name="name"
+      :value="value"
+      :placeholder="placeholder"
+      @input="$emit('input', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "AppInput",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    bigLabel: {
+      type: Boolean,
+      default: false,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
