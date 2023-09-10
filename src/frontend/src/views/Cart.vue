5115e9e4e09e7c653e37a91098f0f4376e020855
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-09-10 16:05:39 +0300

    Создает CartAdditional

--- src/frontend/src/views/Cart.vue
+++ src/frontend/src/views/Cart.vue
@@ -9,124 +9,7 @@
         <template v-if="notEmpty">
           <CartList />
 
-          <div class="cart__additional">
-            <ul class="additional-list">
-              <li class="additional-list__item sheet">
-                <p class="additional-list__description">
-                  <img
-                    src="@/assets/img/cola.svg"
-                    width="39"
-                    height="60"
-                    alt="Coca-Cola 0,5 литра"
-                  />
-                  <span>Coca-Cola 0,5 литра</span>
-                </p>
-
-                <div class="additional-list__wrapper">
-                  <div class="counter additional-list__counter">
-                    <button
-                      type="button"
-                      class="counter__button counter__button--minus"
-                    >
-                      <span class="visually-hidden">Меньше</span>
-                    </button>
-                    <input
-                      type="text"
-                      name="counter"
-                      class="counter__input"
-                      value="2"
-                    />
-                    <button
-                      type="button"
-                      class="counter__button counter__button--plus counter__button--orange"
-                    >
-                      <span class="visually-hidden">Больше</span>
-                    </button>
-                  </div>
-
-                  <div class="additional-list__price">
-                    <b>× 56 ₽</b>
-                  </div>
-                </div>
-              </li>
-              <li class="additional-list__item sheet">
-                <p class="additional-list__description">
-                  <img
-                    src="@/assets/img/sauce.svg"
-                    width="39"
-                    height="60"
-                    alt="Острый соус"
-                  />
-                  <span>Острый соус</span>
-                </p>
-
-                <div class="additional-list__wrapper">
-                  <div class="counter additional-list__counter">
-                    <button
-                      type="button"
-                      class="counter__button counter__button--minus"
-                    >
-                      <span class="visually-hidden">Меньше</span>
-                    </button>
-                    <input
-                      type="text"
-                      name="counter"
-                      class="counter__input"
-                      value="2"
-                    />
-                    <button
-                      type="button"
-                      class="counter__button counter__button--plus counter__button--orange"
-                    >
-                      <span class="visually-hidden">Больше</span>
-                    </button>
-                  </div>
-
-                  <div class="additional-list__price">
-                    <b>× 30 ₽</b>
-                  </div>
-                </div>
-              </li>
-              <li class="additional-list__item sheet">
-                <p class="additional-list__description">
-                  <img
-                    src="@/assets/img/potato.svg"
-                    width="39"
-                    height="60"
-                    alt="Картошка из печи"
-                  />
-                  <span>Картошка из печи</span>
-                </p>
-
-                <div class="additional-list__wrapper">
-                  <div class="counter additional-list__counter">
-                    <button
-                      type="button"
-                      class="counter__button counter__button--minus"
-                    >
-                      <span class="visually-hidden">Меньше</span>
-                    </button>
-                    <input
-                      type="text"
-                      name="counter"
-                      class="counter__input"
-                      value="2"
-                    />
-                    <button
-                      type="button"
-                      class="counter__button counter__button--plus counter__button--orange"
-                    >
-                      <span class="visually-hidden">Больше</span>
-                    </button>
-                  </div>
-
-                  <div class="additional-list__price">
-                    <b>× 56 ₽</b>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
+          <CartAdditional />
 
           <div class="cart__form">
             <div class="cart-form">
@@ -201,10 +84,11 @@
 <script>
 import { mapState } from "vuex";
 import CartList from "@/modules/cart/components/CartList.vue";
+import CartAdditional from "@/modules/cart/components/CartAdditional.vue";
 
 export default {
   name: "CartPage",
-  components: { CartList },
+  components: { CartList, CartAdditional },
   computed: {
     ...mapState("Cart", ["pizzas"]),
 
