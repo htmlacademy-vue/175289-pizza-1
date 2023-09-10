4e4542316dad5bb9b1ac5df40a35aa3fb72d21e1
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-09-10 23:06:50 +0300

    Глобально регистрирует AppInput

--- src/frontend/src/plugins/ui.js
+++ src/frontend/src/plugins/ui.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
 import AppButton from "@/common/components/AppButton.vue";
+import AppInput from "@/common/components/AppInput.vue";
 
 Vue.component("AppButton", AppButton);
+Vue.component("AppInput", AppInput);
