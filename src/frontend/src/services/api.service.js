51893e147b5ba9310c8bdd126dc74d11bb441d19
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-09-27 20:50:50 +0300

    Создает Api сервис

---
+++ src/frontend/src/services/api.service.js
@@ -0,0 +1,85 @@
+import axios from "@/plugins/axios";
+import JwtService from "@/services/jwt.service";
+
+class ApiService {
+  constructor() {}
+}
+
+export class AuthApiService extends ApiService {
+  constructor() {
+    super();
+  }
+
+  login(data) {
+    return axios.post("login", data);
+  }
+
+  logout() {
+    return axios.delete("logout");
+  }
+
+  getMe() {
+    return axios.get("whoAmI");
+  }
+
+  setAuthHeader() {
+    const token = JwtService.getToken();
+
+    axios.defaults.headers.common["Authorization"] = token
+      ? `Bearer ${token}`
+      : "";
+  }
+}
+
+export class ReadOnlyApiService extends ApiService {
+  #resource;
+
+  constructor(resource) {
+    super();
+    this.#resource = resource;
+  }
+
+  /**
+   * Запрос на получения списка сущностей
+   */
+  query() {
+    return axios.get(this.#resource);
+  }
+
+  /**
+   * Запрос на получения одной сущности
+   */
+  get(id) {
+    return axios.get(`${this.#resource}/${id}`);
+  }
+}
+
+export class CrudApiService extends ReadOnlyApiService {
+  #resource;
+
+  constructor(resource) {
+    super(resource);
+    this.#resource = resource;
+  }
+
+  /**
+   * Запрос на создание сущности
+   */
+  post(data) {
+    return axios.post(this.#resource, data);
+  }
+
+  /**
+   * Запрос на обновление сущности
+   */
+  put(data) {
+    return axios.put(`${this.#resource}/${data.id}`, data);
+  }
+
+  /**
+   * Запрос на удаление сущности
+   */
+  delete(id) {
+    return axios.delete(`${this.#resource}/${id}`);
+  }
+}
