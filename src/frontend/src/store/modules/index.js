c4a861c82467fe8459516e4aa47e6fd5e22028d6
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-10-02 08:26:10 +0300

    Создает addresses store

--- src/frontend/src/store/modules/index.js
+++ src/frontend/src/store/modules/index.js
@@ -1,4 +1,5 @@
 import Auth from "@/store/modules/auth.store";
+import Addresses from "@/store/modules/addresses.store";
 import Builder from "@/store/modules/builder.store";
 import Cart from "@/store/modules/cart.store";
 import Misc from "@/store/modules/misc.store";
@@ -6,6 +7,7 @@ import Orders from "@/store/modules/orders.store";
 
 export default {
   Auth,
+  Addresses,
   Builder,
   Cart,
   Misc,
