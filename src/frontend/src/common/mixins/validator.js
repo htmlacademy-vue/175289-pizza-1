8a4bf068534b5fec391e73966d99411be1cfe6d4
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-11-07 18:21:11 +0300

    Правит email regex

--- src/frontend/src/common/mixins/validator.js
+++ src/frontend/src/common/mixins/validator.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-/* eslint-enable */
+const emailRegex = /^\S+@\S+\.\S+$/;
 
 const rules = {
   required: {
