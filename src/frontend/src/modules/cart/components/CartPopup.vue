d1d62df8084c2329fc7c0d8918db7ae1efa0ce98
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-11-19 19:34:24 +0300

    Дорабатывает верстку попапа

--- src/frontend/src/modules/cart/components/CartPopup.vue
+++ src/frontend/src/modules/cart/components/CartPopup.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="popup">
-    <a class="close" href="#" @click="close">
-      <span class="visually-hidden">Закрыть попап</span>
-    </a>
-    <div class="popup__title">
-      <h2 class="title">Спасибо за заказ</h2>
-    </div>
-    <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
-    <div class="popup__button">
-      <a class="button" href="#" @click="close">Отлично, я жду!</a>
+    <div class="popup__content">
+      <a class="close" href="#" @click="close">
+        <span class="visually-hidden">Закрыть попап</span>
+      </a>
+      <div class="popup__title">
+        <h2 class="title">Спасибо за заказ</h2>
+      </div>
+      <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
+      <div class="popup__button">
+        <a class="button" href="#" @click="close">Отлично, я жду!</a>
+      </div>
     </div>
   </div>
 </template>
