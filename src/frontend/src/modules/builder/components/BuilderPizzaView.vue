a02ba363a92b7fc052ac34fbb2afa9ec10b7781a
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-11-19 16:58:47 +0300

    Реализует анимацию переноса ингредиентов на пиццу

--- src/frontend/src/modules/builder/components/BuilderPizzaView.vue
+++ src/frontend/src/modules/builder/components/BuilderPizzaView.vue
@@ -3,18 +3,20 @@
     :class="`pizza pizza--foundation--${pizza.dough.value}-${pizza.sauce.value}`"
   >
     <div class="pizza__wrapper">
-      <template v-for="ingredient in pizza.ingredients">
-        <div
-          v-for="quantity in ingredient.quantity"
-          :key="`${ingredient.value}-layer-${quantity}`"
-          :class="[
-            'pizza__filling',
-            `pizza__filling--${ingredient.value}`,
-            { 'pizza__filling--second': quantity === 2 },
-            { 'pizza__filling--third': quantity === 3 },
-          ]"
-        />
-      </template>
+      <transition-group name="pizza-layer">
+        <template v-for="ingredient in pizza.ingredients">
+          <div
+            v-for="quantity in ingredient.quantity"
+            :key="`${ingredient.value}-layer-${quantity}`"
+            :class="[
+              'pizza__filling',
+              `pizza__filling--${ingredient.value}`,
+              { 'pizza__filling--second': quantity === 2 },
+              { 'pizza__filling--third': quantity === 3 },
+            ]"
+          />
+        </template>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -29,3 +31,20 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.pizza-layer-enter,
+.pizza-layer-leave-to {
+  opacity: 0;
+  transform: scale(1.2);
+}
+.pizza-layer-enter-active,
+.pizza-layer-leave-active {
+  transition-property: opacity, transform;
+  transition-duration: 0.5s;
+  transition-timing-function: ease;
+}
+.pizza-layer-leave-active {
+  transition-duration: 0.2s;
+}
+</style>
