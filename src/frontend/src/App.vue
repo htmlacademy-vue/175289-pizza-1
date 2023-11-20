4911ebf0ce50df35a5d77b0dc37a191eb83385bd
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-11-20 14:24:39 +0300

    Реализует анимацию переходов между экранами

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -3,19 +3,32 @@
     <AppNotifications />
 
     <AppLayout>
-      <router-view />
+      <transition
+        name="view"
+        :appear="appear"
+        enter-active-class="animate__animated animate__slideInLeft"
+      >
+        <router-view />
+      </transition>
     </AppLayout>
   </div>
 </template>
 
 <script>
 import AppLayout from "@/layouts/AppLayout";
+import { AppRoute } from "@/common/constants";
+import "animate.css";
 
 export default {
   name: "App",
   components: { AppLayout },
   created() {
     this.$store.dispatch("init");
+  },
+  computed: {
+    appear() {
+      return this.$route.path !== AppRoute.LOGIN;
+    },
   },
 };
 </script>
