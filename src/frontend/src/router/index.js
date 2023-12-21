61b11210dfb1bca998b8d0cde1c18884930c8dc0
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-12-21 15:28:49 +0300

    Обновляет подключение маршрутизатора в router/index.js

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -7,28 +7,30 @@ import routes from "./routes";
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: "history",
-  routes,
-});
-
-router.beforeEach((to, from, next) => {
-  const middlewares = to.meta.middlewares;
-  if (!middlewares?.length) {
-    return next();
-  }
-
-  const context = { to, from, next, store };
-  const firstMiddlewareIndex = 0;
-  const nextMiddlewareIndex = 1;
-  return middlewares[firstMiddlewareIndex]({
-    ...context,
-    nextMiddleware: middlewarePipeline(
-      context,
-      middlewares,
-      nextMiddlewareIndex
-    ),
+export default Promise.all(routes).then((routes) => {
+  const router = new VueRouter({
+    mode: "history",
+    routes,
   });
-});
 
-export default router;
+  router.beforeEach((to, from, next) => {
+    const middlewares = to.meta.middlewares;
+    if (!middlewares?.length) {
+      return next();
+    }
+
+    const context = { to, from, next, store };
+    const firstMiddlewareIndex = 0;
+    const nextMiddlewareIndex = 1;
+    return middlewares[firstMiddlewareIndex]({
+      ...context,
+      nextMiddleware: middlewarePipeline(
+        context,
+        middlewares,
+        nextMiddlewareIndex
+      ),
+    });
+  });
+
+  return router;
+});
