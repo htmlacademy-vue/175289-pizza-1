e7bef95866f39e4af06c8ca8acb1c851b5648fa1
Юлия Ситникова <yu.sitnikova@inbox.ru>
2023-12-21 15:22:24 +0300

    Обновляет подключение маршрутизатора в main.js

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,14 +1,20 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import store from "@/store";
 import "@/plugins/ui";
 import "@/plugins/vuePlugins";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+const init = async () => {
+  const routerModule = await import("@/router");
+  const router = await routerModule.default;
+
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount("#app");
+};
+
+init();
